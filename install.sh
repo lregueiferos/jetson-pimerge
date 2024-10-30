78b35a00daae431c4f22efd22890306d47322b09
lregueiferos <124931111+lregueiferos@users.noreply.github.com>
2024-10-30 10:52:34 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,38 @@
 #!/bin/bash
-set -e  # Any error will cause the script to fail
+# All stdout here is redirected to /dev/null, but leave stderr alone!
+set -e # Any error will cause the script to fail
+
+echo "Cloning submodules..."
+git submodule update --init > /dev/null
+
+echo "Building Subsystems..."
+cd Subsystems/src
+make >/dev/null
+cd ../..
+
+echo "Compiling the Subsystems program. This could take about 1 minute..."
+cd Subsystems
+dart pub get --offline > /dev/null
+dart compile exe bin/subsystems.dart -o ~/subsystems.exe > /dev/null
+cd ..
+
+echo "Installing configuration files..."
+sudo cp ./11-subsystems.rules /etc/udev/rules.d
+sudo cp ./subsystems.service /etc/systemd/system
+sudo systemctl enable subsystems > /dev/null
+sudo systemctl start subsystems > /dev/null
+sudo udevadm control --reload-rules
+sudo udevadm trigger
+
+echo ""
+echo "Done! Here's what just happened"
+echo "- SocketCan and libserialport were built and installed as dynamic libraries"
+echo "- The subsystems program was compiled to ~/subsystems.exe"
+echo "- udev will auto-detect the IMU and GPS when plugged in"
+echo "- systemd will auto-start the subsystems program on boot"
+
+#!/bin/bash
+
 
 # Redirects output on &3 to /dev/null unless -v or --verbose is passed 
 if [ "$1" = "-v" ]; then
@@ -47,35 +80,4 @@ echo "- The video program was compiled to ~/video.exe"
 echo "- udev will auto-detect cameras when plugged in"
 echo "- systemd will auto-start the video program on boot"
 
-#!/bin/bash
-# All stdout here is redirected to /dev/null, but leave stderr alone!
-set -e  # Any error will cause the script to fail
 
-echo "Cloning submodules..."
-git submodule update --init > /dev/null
-
-echo "Building Subsystems..."
-cd Subsystems/src
-make >/dev/null
-cd ../..
-
-echo "Compiling the Subsystems program. This could take about 1 minute..."
-cd Subsystems
-dart pub get --offline > /dev/null
-dart compile exe bin/subsystems.dart -o ~/subsystems.exe > /dev/null
-cd ..
-
-echo "Installing configuration files..."
-sudo cp ./11-subsystems.rules /etc/udev/rules.d
-sudo cp ./subsystems.service /etc/systemd/system
-sudo systemctl enable subsystems > /dev/null
-sudo systemctl start subsystems > /dev/null
-sudo udevadm control --reload-rules
-sudo udevadm trigger
-
-echo ""
-echo "Done! Here's what just happened"
-echo "- SocketCan and libserialport were built and installed as dynamic libraries"
-echo "- The subsystems program was compiled to ~/subsystems.exe"
-echo "- udev will auto-detect the IMU and GPS when plugged in"
-echo "- systemd will auto-start the subsystems program on boot"
